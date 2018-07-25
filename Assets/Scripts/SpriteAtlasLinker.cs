4de8e03e5f4f9b7c0130ef0f507a5bd63321e007
Tetsuya Mori <monry84@gmail.com>
2018-07-25 13:19:45 +0900

    :sparkles: Implement script

---
+++ Assets/Scripts/SpriteAtlasLinker.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.U2D;
+
+namespace UnityModule
+{
+    [PublicAPI]
+    public class SpriteAtlasLinker : MonoBehaviour
+    {
+        [SerializeField] private List<SpriteAtlas> spriteAtlasList;
+        public List<SpriteAtlas> SpriteAtlasList => spriteAtlasList;
+    }
+}
